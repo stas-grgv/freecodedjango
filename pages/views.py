85c82f669c449e0c25685c5cdf59db37fd398c45
stas-grgv <77362690+stas-grgv@users.noreply.github.com>
2021-07-02 18:35:53 +0300

    Minor changes

--- pages/views.py
+++ pages/views.py
@@ -14,6 +14,6 @@ def about_view(request, *args, **kwargs):
     my_context = {
         "my_text":"this is about me",
         "my_number":123,
-        "my_list": [123,42,24]
+        "my_list": [123,42,24, 'bce']
     }
     return render(request, "about.html", my_context)
