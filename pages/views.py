f1aec276a97e5f5308bf3bbf6d71b34965b30820
stas-grgv <77362690+stas-grgv@users.noreply.github.com>
2021-07-02 23:04:50 +0300

    Static render from db

--- pages/views.py
+++ pages/views.py
@@ -10,10 +10,11 @@ def home_view(request, *args, **kwargs):
 def contact_view(request, *args, **kwargs):
     return render(request, "contact.html", {})
 
+
 def about_view(request, *args, **kwargs):
     my_context = {
-        "title":"this is about me",
-        "my_number":123,
-        "my_list": [123,42,24, 'bce']
+        "title": "this is about me",
+        "my_number": 123,
+        "my_list": [123, 42, 24, 'bce']
     }
     return render(request, "about.html", my_context)
