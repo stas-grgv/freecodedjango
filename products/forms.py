406419aa4c48b2111df5ccc695b76962d88ee70c
stas-grgv <77362690+stas-grgv@users.noreply.github.com>
2021-07-05 01:03:58 +0300

    Pure django form for Product

--- products/forms.py
+++ products/forms.py
@@ -11,3 +11,9 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "Your title"}))
+    description = forms.CharField(required=False, widget=forms.Textarea(attrs={"class": "new-class-name", "rows": 14}))
+    price = forms.DecimalField()
--- products/views.py
+++ products/views.py
@@ -1,31 +1,29 @@
 from django.shortcuts import render
 
 from .models import Product
-from .forms import ProductForm
+from .forms import RawProductForm
 
 
 # View with product detail. Now with id 1
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
-    # context = {
-    #     "title": obj.title,
-    #     "description": obj.description,
-    #     "price":obj.price,
-    # }
     context = {
         'object': obj,
     }
     return render(request, 'product/details.html', context)
 
 
-# View with create product form
-def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
+def product_create_view(reqest):
+    my_form = RawProductForm()
+    if reqest.method == 'POST':
+        my_form = RawProductForm(reqest.POST)
+        if my_form.is_valid():
+            print(my_form.cleaned_data)
+            # Kwargs
+            Product.objects.create(**my_form.cleaned_data)
+            my_form = RawProductForm()
+        else:
+            print(my_form.errors)
 
-    context = {
-        'form': form
-    }
-    return render(request, "product/product-create.html", context)
+    context = {"form": my_form}
+    return render(reqest, 'product/product-create.html', context)
