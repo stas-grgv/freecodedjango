08bbb689a9086c0fcf0e8b1f666d18fa69f40bd6
stas-grgv <77362690+stas-grgv@users.noreply.github.com>
2021-07-08 21:06:32 +0300

    minor changes

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blog.views import articles_list
+from blog.views import articles_list, article_details
 
 app_name = 'blog'
 urlpatterns = [
     path('', articles_list, name='articles list'),
+    path('<int:id>', article_details, name='article details')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
+from django.http import Http404
 from django.shortcuts import render
-
 
 # Create your views here.
 from blog.models import Article
@@ -9,3 +9,14 @@ def articles_list(request):
     articles = Article.objects.all()
     context = {'a': 4, 'articles': articles}
     return render(request, 'article/articles-list.html', context)
+
+
+def article_details(request, id):
+    try:
+        article = Article.objects.get(id=id)
+    except Article.DoesNotExist:
+        raise Http404
+    context = {
+        "article": article,
+    }
+    return render(request, 'article/article-details.html', context)
